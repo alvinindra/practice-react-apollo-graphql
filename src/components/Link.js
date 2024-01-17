94f9a788715045b826f96f8305d29ebb6d6d6b8f
alvinindra <alvin@antikode.com>
2024-01-17 14:42:04 +0700

    feat: add pagination

--- src/components/Link.js
+++ src/components/Link.js
@@ -1,8 +1,12 @@
 import { useMutation, gql } from '@apollo/client'
 import React from 'react'
-import { AUTH_TOKEN } from '../constants'
+import { AUTH_TOKEN, LINKS_PER_PAGE } from '../constants'
 import { timeDifferenceForDate } from '../utils'
 import { FEED_QUERY } from './LinkList'
+
+const take = LINKS_PER_PAGE
+const skip = 0
+const orderBy = { createdAt: 'desc' }
 
 const VOTE_MUTATION = gql`
   mutation VoteMutation($linkId: ID!) {
@@ -35,6 +39,11 @@ const Link = (props) => {
     update: (cache, { data: { vote } }) => {
       const { feed } = cache.readQuery({
         query: FEED_QUERY,
+        variables: {
+          take,
+          skip,
+          orderBy,
+        },
       })
 
       const updatedLinks = feed.links.map((feedLink) => {
@@ -53,6 +62,11 @@ const Link = (props) => {
           feed: {
             links: updatedLinks,
           },
+        },
+        variables: {
+          take,
+          skip,
+          orderBy,
         },
       })
     },
